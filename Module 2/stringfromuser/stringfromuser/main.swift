016b49bff367aca7eb8a99f7247c0059bdfc4f49
Mukund-Vaghasiya13 <115715969+Mukund-Vaghasiya13@users.noreply.github.com>
2022-11-24 20:55:41 +0530

    Add files via upload

---
+++ Module 2/stringfromuser/stringfromuser/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  stringfromuser
+//
+//  Created by Mukund vaghasiya  on 24/11/22.
+//
+
+import Foundation
+
+var str = ""
+print("Enter the value of string:")
+
+str = readLine()!
+
+print("String:\(str)")
+
+
