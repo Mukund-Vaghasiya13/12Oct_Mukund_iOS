496039e81bc8d059b3b5bb048d35b9af8d36b1d9
Mukund-Vaghasiya13 <115715969+Mukund-Vaghasiya13@users.noreply.github.com>
2022-11-25 18:53:20 +0530

    Add files via upload

---
+++ Module 2/arithmetic operation/arithmetic operation/main.swift
@@ -0,0 +1,29 @@
+//
+//  main.swift
+//  arithmetic operation
+//
+//  Created by Mukund vaghasiya  on 25/11/22.
+//
+
+import Foundation
+
+print("Enter the value of a:")
+var a = Int(readLine()!)!
+
+print("Enter the value of b:")
+var b = Int(readLine()!)!
+
+// arithmetic opration
+
+print("Addition: \(a+b)")
+
+print("Subtraction:\(a-b)")
+
+print("Multiplication:\(a*b)")
+
+print("Division:\(a/b)")
+
+print("Modulo opretor (Reminder):\(a%b)")
+
+
+
