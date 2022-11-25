88f5bd29ad821b9d5063a83566ab0fcd3d00d98c
Mukund-Vaghasiya13 <115715969+Mukund-Vaghasiya13@users.noreply.github.com>
2022-11-25 18:27:53 +0530

    Add files via upload

---
+++ Module 2/default value/default value/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  default value
+//
+//  Created by Mukund vaghasiya  on 24/11/22.
+//
+
+import Foundation
+
+var number:Int = 10
+
+print("Default value of number:\(number)")
+
