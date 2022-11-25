d813c85694fbdae84b10c1489a2e58b6b385d480
Mukund-Vaghasiya13 <115715969+Mukund-Vaghasiya13@users.noreply.github.com>
2022-11-25 18:20:00 +0530

    Add files via upload

---
+++ Module 2/int from user/int from user/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  int from user
+//
+//  Created by Mukund vaghasiya  on 24/11/22.
+//
+
+import Foundation
+
+
+var number:Int
+print("Enter the number:")
+number = Int(readLine()!)!
+
+print("Number is : \(number)")
+
+
