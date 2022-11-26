6318534e11845cdbb7a062d04b5ccc03d1004d11
Mukund-Vaghasiya13 <115715969+Mukund-Vaghasiya13@users.noreply.github.com>
2022-11-26 19:38:34 +0530

    Add files via upload

---
+++ Module 2/convertion of int/convertion of int/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  convertion of int
+//
+//  Created by Mukund vaghasiya  on 26/11/22.
+//
+
+import Foundation
+
+var a:Int = 0
+
+print("Enter the value of a:")
+a = Int(readLine()!)!
+
+var b:Float = Float(a)
+print("Integer to float value : \(b)")
+
+
