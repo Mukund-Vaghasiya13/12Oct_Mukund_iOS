53e621a0d5128453d3d9dd3e5c3c92e5c30631c5
Mukund-Vaghasiya13 <115715969+Mukund-Vaghasiya13@users.noreply.github.com>
2022-11-26 19:44:27 +0530

    Add files via upload

---
+++ Module 2/printmessage/printmessage/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  printmessage
+//
+//  Created by Mukund vaghasiya  on 26/11/22.
+//
+
+import Foundation
+
+var a = 0
+print("Enter the value of a:")
+a = Int(readLine()!)!
+
+print("The value of a is \(a)")
