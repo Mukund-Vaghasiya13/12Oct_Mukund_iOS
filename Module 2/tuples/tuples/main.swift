b7c4f2fe97f1dfddf1d34c3b1d0f482088da5b05
Mukund-Vaghasiya13 <115715969+Mukund-Vaghasiya13@users.noreply.github.com>
2022-12-21 11:37:27 +0530

    Add files via upload

---
+++ Module 2/tuples/tuples/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  tuples
+//
+//  Created by Mukund vaghasiya  on 06/12/22.
+//
+
+import Foundation
+
+var cityname = ("rajkot","surat","baroda","junaghad","jamnagar")
+
+print(cityname)
+
