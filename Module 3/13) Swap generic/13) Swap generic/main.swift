4afdeeb20036aa0b92737b6d07b9013ad26d81bd
Mukund-Vaghasiya13 <115715969+Mukund-Vaghasiya13@users.noreply.github.com>
2022-12-22 16:45:44 +0530

    Add files via upload

---
+++ Module 3/13) Swap generic/13) Swap generic/main.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  13) Swap generic
+//
+//  Created by Mukund vaghasiya  on 22/12/22.
+//
+
+import Foundation
+
+var temp = 0
+print("enter the value of a:")
+var a = Int(readLine()!)!
+
+print("Enter the value of b:")
+var b = Int(readLine()!)!
+
+temp = a
+a = b
+b = temp
+
+print("a:\(a) & b:\(b)")
+
+
