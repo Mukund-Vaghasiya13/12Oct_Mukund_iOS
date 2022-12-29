22e89c8d9dc97932e83d48fed0a73abe40688e33
Mukund-Vaghasiya13 <115715969+Mukund-Vaghasiya13@users.noreply.github.com>
2022-12-29 15:01:56 +0530

    Add files via upload

---
+++ Module 3/2) dic arr/2) dic arr/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  2) dic arr
+//
+//  Created by Mukund vaghasiya  on 29/12/22.
+//
+
+import Foundation
+var dictonary:[String:[Any]] = ["color":["red","blue","Black","orange"],"degits":[1,2,3,4,5,6,7,8]]
+
+print(dictonary["color"]!)
+print(dictonary["degits"]!)
