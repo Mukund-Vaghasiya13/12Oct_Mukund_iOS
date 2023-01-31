f34a26e7b783559d7d479c3eee1a852bed4f4280
Mukund-Vaghasiya13 <115715969+Mukund-Vaghasiya13@users.noreply.github.com>
2023-01-31 17:26:53 +0530

    Add files via upload

---
+++ module 4/8) value from keyboard/8) value from keyboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  8) value from keyboard
+//
+//  Created by Mukund vaghasiya  on 26/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController{
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+
+    @IBAction func mytextfield(_ sender: UITextField) {
+      
+    }
+}
+
